92a0dc1f5d5bb4384c46cf3ba09a8d648f476fe0
Gabriele Piscitello <s352024@studenti.polito.it>
2026-05-30 10:46:46 +0200

    set up express server with middleware

--- server/index.js
+++ server/index.js
@@ -1,9 +1,33 @@
 // imports
 import express from "express";
+import cors from 'cors';
+import morgan from 'morgan';
+import session from 'express-session';
 
 // init express
 const app = new express();
 const port = 3001;
+
+// --- Middleware ---
+app.use(morgan('dev'));
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
+
+app.use(express.json());
+
+app.use(session({
+  secret: 'lastrace-secret-key',
+  resave: false,
+  saveUninitialized: false,
+}));
+
+// --- Routes ---
+app.get('/api/ping', (req, res) => {
+  res.json({ message: 'ok' });
+});
 
 // activate the server
 app.listen(port, () => {
